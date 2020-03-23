28832c0af93a27fdce608d48af0d58f6e4f9e0f1
Spencer Najib <spencernajib2@gmail.com>
2020-03-23 02:51:22 +0000

    Added all files to remote repo

---
+++ bin/app.js
@@ -0,0 +1,146 @@
+#!/usr/bin/env node
+var program = require('commander')
+var FileServer = require('../lib/server')
+var log = console.log
+var path = require('path')
+var Browser = require('../lib/openBrowser')
+var ps = []
+var killer = require('../lib/processKiller')
+var os = require('os')
+var fs = require('fs')
+// setup sqlite database 
+var db = require('../lib/db')
+
+
+
+// main commands
+program
+ .version(require('../package').version)
+    .command('start')// the main  sub-command name
+    .alias('s')// alternative sub -command name
+    .option("-d,--dirName [value]", 'The directory served')
+    .option("-p,--port [value]", 'The port for server, default port is 7000')
+    .option("-N,--noBrowser [value]",'disable opening the browser, by default,this false')
+    .description(' starts a file server in a given directory or current directory by default')
+    .action(function (options) {
+       var port = options.port===undefined?7000: parseInt(options.port);
+       var dirName = options.dirName===undefined?process.cwd():options.dirName;
+       var noBrowser = options.noBrowser
+      
+// Start 
+var t
+  try {
+     
+      
+     t = FileServer(dirName,port,noBrowser)
+     // when the server is terminated, remove its details from the sqlite database
+      process.on('SIGINT'||'SIGTERM', () => {
+          t.Server.close(function() {
+              db.run('DELETE FROM servers WHERE pid =?', [t.pid], function (err) {
+                  if (err) { throw err }
+                  console.log('fileServer teminated  and database cleared')
+                  db.close()
+              })
+              
+          })
+      })
+      
+    }
+    
+      // open the url in browser
+   
+      
+  catch(err) {
+      throw err
+  }
+  finally{
+    
+      db.serialize(function () {
+          // only create database if the db is doesn't exist and only if the database file is empty
+          if (!fs.existsSync(os.tmpdir() + '/test.db') || fs.readFileSync(os.tmpdir + '/test.db', { encoding: 'utf8' }).length===0) {
+              db.run("CREATE TABLE servers(Serveraddress TEXT,dir TEXT,pid INTEGER)");
+          }
+
+          var stmt = db.prepare("INSERT INTO servers(Serveraddress,dir,pid) VALUES (?,?,?)");
+         stmt.run([t.address,t.dir,t.pid])
+         stmt.finalize()
+         console.log('server address and pid successfully added to database')
+
+
+         
+      });
+
+  
+
+
+  }
+  // when the server is closed or process is exited
+
+    })
+
+/*
+** command thats kills of all the instance of the fileServer
+
+
+*/
+program
+ .command('kill')// the main  sub-command name
+    .alias('k')// alternative sub -command name
+    .description('stops all instances of the fileServer active')
+    .action(function(){
+// if db file is not empty
+        if (fs.readFileSync(os.tmpdir + '/test.db', { encoding: 'utf8' }).length>0) {
+        db.all("SELECT rowid AS id, pid FROM servers",[], function (err, row) {
+         if(row.length<1){
+             console.log('no fileServer processes running')
+         }   
+         ////
+          else {
+          ps = row
+       
+            //var  log(ps) last = row.pop().pid
+           try {
+               console.time('executing')
+            ps.forEach(v=>{
+                killer(v.pid,function(){
+                    db.run ('DELETE FROM servers WHERE pid =?', [v.pid], function (err) {
+                        if (err)  {throw err}
+                           console.log(this.changes)
+                           db.close()
+                    })
+                })
+                
+            })
+               console.timeEnd('executing')
+           
+
+           }
+           catch(err){
+               log(err)
+           }
+            
+        }}
+        )
+    
+    }
+     else {
+          console.log('no fileServer processes running')
+        
+     }
+    })
+
+// let the program parse the CLI arguments
+program.parse(process.argv)
+// error handler 
+
+// in case of no args
+// in case of no args
+
+
+// error on unknown commands
+
+    if (process.argv.length<3) {
+      
+        program.help()
+    
+    }
---
+++ test/server.test.js
@@ -0,0 +1,143 @@
+"use strict";
+
+var Server = require('../lib/server');
+
+var fetch = require('node-fetch');
+
+var fs = require('fs');
+
+var os = require('os');
+
+var tmpFile = os.tmpDir() || os.tmpdir();
+
+var sqlite3 = require('sqlite3').verbose();
+
+var killer = require('../lib/killer-test');
+
+var db = require('../lib/db'); // setup database connection
+//  fetch the database
+
+
+function fetcher(cb) {
+  return db.all('SELECT* FROM servers', [], cb);
+}
+
+describe('fileServer', function () {
+  beforeAll(function () {
+    console.log('ensure  atleast one instance of fileServer is running before you run tests else the tests will fail');
+    jest.setTimeout(10000);
+  }); // the server should started successfully
+
+  it('should start sucessfully', function (done) {
+    fetcher(function (err, row) {
+      if (err || row.length < 1) {
+        expect(row.length > 0).toEqual(true);
+        done(err);
+      }
+
+      row.forEach(function (server) {
+        return fetch(server.Serveraddress).then(function (resp) {
+          expect(resp.status).toBe(200);
+          done();
+        })["catch"](function (err) {
+          expect(true).toBe(false);
+        });
+      });
+    });
+  }); // the server should  serve all contents in passed folder
+  // The all files and folders should show up on the index.html page
+
+  it('should serve  all files in passed directory', function (done) {
+    fetcher(function (err, row) {
+      if (err || row.length < 1) {
+        expect(row.length > 0).toEqual(true);
+        done(err);
+      }
+
+      row.forEach(function (server) {
+        return fs.readdirSync(server.dir).map(function (v) {
+          var arr = [].concat(v);
+
+          if (/^\./g.test(v) && !v.includes('ignore') || !/\.\w+$/g.test(v)) {
+            arr.push('/');
+            return arr.join('');
+          }
+
+          if (/^\./g.test(v) && v.includes('ignore')) {
+            arr.push('/');
+            return arr.join('');
+          }
+
+          return v;
+        }).forEach(function (V) {
+          return fetch(server.Serveraddress).then(function (resp) {
+            return resp.text();
+          }).then(function (txt) {
+            var tes = txt.includes(V);
+            expect(tes).toEqual(true);
+            done();
+          })["catch"](function (err) {
+            expect(true).toBe(false);
+          });
+        });
+      });
+    });
+  }); // the fileServer should  serve all the folders and files in the passed directory
+  // the content contents of each dir should match what's served
+
+  it('should serve the all folders and files', function (done) {
+    fetcher(function (err, row) {
+      if (err || row.length < 1) {
+        expect(row.length > 0).toEqual(true);
+        done(err);
+      }
+
+      row.forEach(function (server) {
+        return fs.readdirSync(server.dir).map(function (v) {
+          var arr = [].concat(v);
+
+          if (/^\./g.test(v) && !v.includes('ignore') || !/\.\w+$/g.test(v)) {
+            arr.push('/');
+            return arr.join('');
+          }
+
+          if (/^\./g.test(v) && v.includes('ignore')) {
+            arr.push('/');
+            return arr.join('');
+          }
+
+          return encodeURI(v);
+        }).forEach(function (V) {
+          return fetch(server.Serveraddress + '/' + V).then(function (resp) {
+            expect(resp.status).toBe(200);
+            done();
+          })["catch"](function (err) {
+            expect(true).toBe(false);
+          });
+        });
+      });
+    });
+  }); // fileServer app should kill all instances of the server which kill command is passed
+
+  it('should successfully stopped the running instances of the fileServer', function (done) {
+    fetcher(function (err, row) {
+      if (err || row.length < 1) {
+        expect(row.length > 0).toEqual(true);
+      } else {
+        row.forEach(function (v) {
+          killer(v.pid, function () {
+            db.run('DELETE FROM servers WHERE pid =?', [v.pid], function (err) {
+              if (err) {
+                expect(true).toBe(false);
+              }
+
+              expect(this.changes > 0).toBe(true);
+              done();
+            });
+          });
+        });
+      }
+    });
+  });
+});
+// 
